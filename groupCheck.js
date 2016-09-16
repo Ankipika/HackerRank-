b25fa8680811ba350eb4bfc92083e654165d5e25
Justin Thomas <jthomas0209@gmail.com>
2016-09-16 13:59:11 -0400

    added codewars excercises to the mix

---
+++ groupCheck.js
@@ -0,0 +1,30 @@
+ function groupCheck(s){
+   console.log(s);
+   var pairs = {'}':'{', ']':'[',')':'('}
+   var bracketStack = [];
+
+   if (s.length === 0){
+     return true;
+   }
+   s = s.split("");
+   
+   for(i = 0; i < s.length; i++){
+     if (openBracketCharCheck(s[i])){
+       bracketStack.unshift(s[i]); 
+     }
+     else if (pairs[s[i]] === bracketStack[0]){
+       bracketStack.shift()
+     }
+     else{
+       return false;
+     }
+   }
+   if (bracketStack.length === 0){
+     return true;
+   }
+   return false;
+   
+   function openBracketCharCheck(s) {
+     return s === '{' || s === '[' || s === '(';
+   }
+ }
